e302123c389eb5cb8a9547e40c0abe89cc75e3f5
Melissa Lee <mellee@riotgames.com>
2023-08-14 22:28:37 -0700

    update base root

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {}
+
+if (isProd) {
+    nextConfig = {
+        basePath: "/",
+    }
+}
 
 module.exports = nextConfig
