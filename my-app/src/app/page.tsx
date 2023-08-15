a2750b3b6e96aeb514fffa7ea95cf55d3f831a59
Melissa Lee <mellee@riotgames.com>
2023-08-14 21:47:53 -0700

    update links

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -29,54 +29,47 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+      <a
+          className={styles.shaded_card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            About
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/melissaflee02/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            LinkedIn
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/melissaflee02"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            Github
           </h2>
-          <p>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="mailto: mlee24@stanford.edu"
           className={styles.card}
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            Email
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
         </a>
       </div>
     </main>
