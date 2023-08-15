d2e35470d5ec17b7afc68c061cf15063f3970f1f
Melissa Lee <mellee@riotgames.com>
2023-08-14 22:33:47 -0700

    update base path

--- next.config.js
+++ next.config.js
@@ -4,10 +4,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 let nextConfig = {}
 
-if (isProd) {
-    nextConfig = {
-        basePath: "/my-app",
-    }
-}
 
 module.exports = nextConfig
