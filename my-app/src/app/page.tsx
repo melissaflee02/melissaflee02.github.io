06abce9c0df64210e9b5aef72053bc4a16f7d826
Melissa Lee <mellee@riotgames.com>
2023-08-14 21:46:36 -0700

    update intro

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,34 +8,24 @@ export default function Home() {
         <p>
          Welcome to my page! &nbsp;
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <a href="../public/Melissa_Lee_Resume.pdf">Resume</a>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <p>
+          <b>Hi! I'm Melissa</b>, a rising senior at <a href="https://www.stanford.edu/" className={styles.link}>Stanford University</a> pursuing 
+          a dual Bachelor's and Master's degree in Computer Science (Artificial Intelligence and Computer Systems). This past summer, 
+          I interned at <a href="https://www.riotgames.com/en" className={styles.link}>Riot Games</a>, architecting and implementing an end-to-end 
+          match simulation framework enabling all game teams across <i>League of Legends</i>, <i>Valorant</i>, and R&D initiatives to iteratively test and 
+          optimize their matchmaking algorithms. This complemented my previous year's experience integrating an industry-leading BERT Natural Language 
+          Processing model into Riot's name-checking pipeline, improving offensive name detection by 50%!<br /> <br />
+
+          In my free time, I enjoy growing the sport of <a href="https://www.wrestling.life/yes-we-can" className={styles.link}>women's wrestling</a> and 
+          learning more about the programming world through independent coding projects. Feel free to check out my <a href="https://github.com/melissaflee02" className={styles.link}>Github</a>!<br /> <br />
+
+          Having struggled with social interaction at a young age, I am passionate about leveraging technology to advance the human interaction
+          model. In pursuit of this goal, I am exploring ways AI can facilitate, and participate in, healthy human interaction.
+        </p>
       </div>
 
       <div className={styles.grid}>
